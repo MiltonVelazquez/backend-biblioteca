3db0ec09675dc19bac97889d23671125ac979f07
Milton Velazquez <133221591+MiltonVelazquez@users.noreply.github.com>
2025-10-28 16:40:35 -0300

    Update PrestamoRepository.java

--- src/main/java/mds/biblioteca/repository/PrestamoRepository.java
+++ src/main/java/mds/biblioteca/repository/PrestamoRepository.java
@@ -2,16 +2,15 @@
 package mds.biblioteca.repository;
 
 import mds.biblioteca.model.Libro;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import mds.biblioteca.model.Prestamo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface PrestamoRepository extends JpaRepository<Prestamo, Long> {
 
-    /**
-     * Para el flujo "Devolución": "¿Prestamo existe?".
-     * Busca un préstamo activo (Fecha_Devolucion es null) para un libro específico.
-     * Así sabemos qué préstamo estamos cerrando.
-     */
     Optional<Prestamo> findByLibroAndFechaDevolucionIsNull(Libro libro);
+
+    Page<Prestamo> findByFechaDevolucionIsNotNull(Pageable pageable);
 }
