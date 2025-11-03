0c5d34986cd043f4489094f6ded7006b2a9133ac
Milton Velazquez <133221591+MiltonVelazquez@users.noreply.github.com>
2025-11-03 17:01:23 -0300

    Update DevolucionRequestDto.java

--- src/main/java/mds/biblioteca/dto/DevolucionRequestDto.java
+++ src/main/java/mds/biblioteca/dto/DevolucionRequestDto.java
@@ -1,5 +1,6 @@
+
 package mds.biblioteca.dto;
-import jakarta.validation.constraints.NotBlank;
+
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,7 +8,8 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 public class DevolucionRequestDto {
-    Long idLibro;
-    String nroSocio;
-    Boolean buenasCondiciones;
+    // ¡AÑADIR private Y @NotNull!
+    @NotNull private Long idLibro;
+    @NotNull private String nroSocio; // @NotBlank también es válido
+    @NotNull private Boolean buenasCondiciones;
 }
