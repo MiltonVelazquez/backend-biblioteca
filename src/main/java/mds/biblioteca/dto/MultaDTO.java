59209e584d8a794e51cb01fcf9f4586c3c62b064
Milton Velazquez <133221591+MiltonVelazquez@users.noreply.github.com>
2025-11-03 17:01:53 -0300

    Update MultaDTO.java

--- src/main/java/mds/biblioteca/dto/MultaDTO.java
+++ src/main/java/mds/biblioteca/dto/MultaDTO.java
@@ -8,11 +8,11 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 public class MultaDTO{ 
-    Long idMulta;
-    Double monto;
-    Long idPrestamo;
-    String socioNombreCompleto;
-    String socioNroSocio;
-    boolean pagada;
-    LocalDate fechaGeneracion;
+    private Long idMulta;
+    private Double monto;
+    private Long idPrestamo;
+    private String socioNombreCompleto;
+    private String socioNroSocio;
+    private boolean pagada;
+    private LocalDate fechaGeneracion;
 }
